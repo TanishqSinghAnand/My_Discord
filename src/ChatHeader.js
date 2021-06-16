1b6c20538d47857e0345cb17b6443240d98aeb38
Tanishq Singh Anand <anandtanishqs@gmail.com>
2021-06-16 21:30:30 +0530

    Changed Logo

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -8,31 +8,32 @@ import SendRoundedIcon from "@material-ui/icons/SendRounded";
 import HelpRoundedIcon from "@material-ui/icons/HelpRounded";
 
 function ChatHeader({ channelName }) {
-
-    return (
-        <div className="chatHeader">
-            <div className="chatHeader__left">
-                <h3>
-                    <span className="chatHeader__hash">
-                        #
-            </span>
-            {channelName}
-        </h3>
-            </div>
-            <div className="chatHeader__right">
-                <NotificationsIcon />
-                <EditLocationRoundedIcon />
-                <PeopleAltRoundedIcon />
-
-                <div className="chatHeader__search">
-                    <input placeholder="Search" />
-                    <SearchRoundedIcon />
-                </div>
-                <SendRoundedIcon />
-                <HelpRoundedIcon />
-            </div>
-        </div>
-    );
-}
-
-export default ChatHeader;
+                    
+    return (                          
+        <div className="chatHeader">                          
+            <div className="chatHeader__left">                          
+                <h3>                          
+                    <span className="chatHeader__hash">                          
+                        #                          
+            </span>                          
+            {channelName}                          
+        </h3>                          
+            </div>                          
+            <div className="chatHeader__right">                          
+                <NotificationsIcon />                          
+                <EditLocationRoundedIcon />                          
+                <PeopleAltRoundedIcon />                          
+                    
+                <div className="chatHeader__search">                          
+                    <input placeholder="Search" />                          
+                    <SearchRoundedIcon />                          
+                </div>                          
+                <SendRoundedIcon />                          
+                <HelpRoundedIcon />                          
+            </div>                          
+        </div>                          
+    );                          
+}                          
+                    
+export default ChatHeader;                          
+                    
--- src/Login.js
+++ src/Login.js
@@ -10,7 +10,7 @@ function Login() {
     return (
         <div className="login">
             <div className="login__logo">
-                <img src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/800px-Discord_logo.svg.png" alt="Discord Logo"/>
+                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/98/Discord_logo.svg/2000px-Discord_logo.svg.png" alt="Discord Logo"/>
 
             </div>
             <Button onClick={signIn}>Sign In</Button>
